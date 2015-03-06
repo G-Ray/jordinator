a54e55475ac3252938d727fd823008e9e8818673
G-Ray <geoffrey.bonneville@gmail.com>
2015-03-06 16:32:07 +0100

    Print Jordinator

--- jordinator/src/org/eidd/jordinator/EV3HelloWorld.java
+++ jordinator/src/org/eidd/jordinator/EV3HelloWorld.java
@@ -1,31 +1,28 @@
 package org.eidd.jordinator;
 import lejos.hardware.BrickFinder;
 import lejos.hardware.Button;
-import lejos.hardware.Sound;
 import lejos.hardware.lcd.Font;
 import lejos.hardware.lcd.GraphicsLCD;
 import lejos.utility.Delay;
 
 public class EV3HelloWorld
 {
+    private static String[] melody = { "C4", "D4", "E4", "C4",
+        "E4", "C4",   "E4"};
+
     public static void main(String[] args)
     {
         //System.out.println("Running...");
         GraphicsLCD g = BrickFinder.getDefault().getGraphicsLCD();
         final int SW = g.getWidth();
         final int SH = g.getHeight();
-        Button.LEDPattern(4);
-        Sound.beepSequenceUp();
-        
+        Button.LEDPattern(0);
+
         g.setFont(Font.getLargeFont());
-        g.drawString("leJOS/EV3", SW/2, SH/2, GraphicsLCD.BASELINE|GraphicsLCD.HCENTER);
-        Button.LEDPattern(3);
+        g.drawString("Jordinator", SW/2, SH/2, GraphicsLCD.BASELINE|GraphicsLCD.HCENTER);
+        Button.LEDPattern(0);
         Delay.msDelay(4000);
-        Button.LEDPattern(5);
         g.clear();
         g.refresh();
-        Sound.beepSequence();
-        Delay.msDelay(500);
-        Button.LEDPattern(0);
     }
 }

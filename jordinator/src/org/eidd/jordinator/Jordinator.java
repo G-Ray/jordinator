41d6d3ec55da1a223475da3c3b574bb91786e890
G-Ray <geoffrey.bonneville@gmail.com>
2015-03-06 16:45:09 +0100

    Rename Main class to Jordinator

--- jordinator/src/org/eidd/jordinator/Jordinator.java
+++ jordinator/src/org/eidd/jordinator/Jordinator.java
@@ -5,22 +5,18 @@ import lejos.hardware.lcd.Font;
 import lejos.hardware.lcd.GraphicsLCD;
 import lejos.utility.Delay;
 
-public class EV3HelloWorld
+public class Jordinator
 {
-    private static String[] melody = { "C4", "D4", "E4", "C4",
-        "E4", "C4",   "E4"};
-
     public static void main(String[] args)
     {
         //System.out.println("Running...");
         GraphicsLCD g = BrickFinder.getDefault().getGraphicsLCD();
         final int SW = g.getWidth();
         final int SH = g.getHeight();
-        Button.LEDPattern(0);
+        Button.LEDPattern(5);
 
         g.setFont(Font.getLargeFont());
         g.drawString("Jordinator", SW/2, SH/2, GraphicsLCD.BASELINE|GraphicsLCD.HCENTER);
-        Button.LEDPattern(0);
         Delay.msDelay(4000);
         g.clear();
         g.refresh();
